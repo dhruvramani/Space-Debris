71c188c8e4b61b4e52af9182520a9083be64ba5f
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-03-07 22:12:03 +0530

    ughh

--- src/dataset.py
+++ src/dataset.py
@@ -73,11 +73,11 @@ class DebrisDataset(Dataset):
     def __getitem__(self, idx):
         debris_name = self.names[idx]
         sample = np.array(self.elem[debris_name]).astype(np.float32)
-        sequences = []
-        predictions = []
-        for i in range(0, 10):
-            sequences.append(sample[i : i+ self.steps])
-            predictions.append(sample[i + self.steps: i + self.steps + 1])
+        sequences = sample[0 : self.steps]
+        predictions = sample[self.steps: self.steps + 1]
+        for i in range(1, 10):
+            sequences = np.concatenate((sequences, sample[i : i+ self.steps]), axis=0)
+            predictions = np.concatenate((sample[i + self.steps: i + self.steps + 1]), axis=0)
         return np.asarray(sequences), np.asarray(predictions)
 
     
