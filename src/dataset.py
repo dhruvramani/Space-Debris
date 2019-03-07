3472d984c1533fb13911abc0e3004f9f3cfd7718
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-03-07 21:31:51 +0530

    ughh

--- src/dataset.py
+++ src/dataset.py
@@ -88,4 +88,4 @@ if __name__ == '__main__':
     for i in range(0, len(dataloader)):
         (sequences, predictions) = next(dataloader)
         print(sequences, predictions)
-        
+        break
