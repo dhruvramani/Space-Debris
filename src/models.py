22bb3804ad55dd9a4bf6e58e9fff906b0dc58207
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-03-08 00:53:07 +0530

    ughh

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ from torch.autograd import Variable
 torch.manual_seed(1)
 
 class SpaceLSTM(torch.nn.Module):
-    def __init__(self, inp_dim=6, hidden_dim=4, op_dim=6, batch_size=128):
+    def __init__(self, inp_dim=6, hidden_dim=6, op_dim=6, batch_size=128):
         super(SpaceLSTM, self).__init__()
         # Hidden I/P : 2 
         self.batch_size, self.hidden_dim = batch_size, hidden_dim
